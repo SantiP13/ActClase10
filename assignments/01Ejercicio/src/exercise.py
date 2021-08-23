5bc2c72895e87d3c4c95710c980835723a6585fa
SantiP13 <89026993+SantiP13@users.noreply.github.com>
2021-08-23 08:34:19 -0700

    Update exercise.py

--- assignments/01Ejercicio/src/exercise.py
+++ assignments/01Ejercicio/src/exercise.py
@@ -1,6 +1,23 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    def horas(hr, mn, s):
+        s += hr * 3600 + mn * 60
+        return s
+
+    print("proceso 1:")
+
+    hr = int(input("Introduce las horas: "))
+    mn = int(input("Introduce los minutos: "))
+    s = int(input("Introduce los segundos: "))
+
+    print("\nproceso 2:")
+
+    hr2 = int(input("Introduce las horas: "))
+    mn2 = int(input("Introduce los minutos: "))
+    s2 = int(input("Introduce los segundos: "))
+
+    print("proceso 1:", horas(hr, mn, s))
+    print("proceso 2:", horas(hr2, mn2, s2))
 
 if __name__ == '__main__':
     main()
